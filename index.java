500f9cac7df73e1989512a175396ecd8ec326826
sureshjoshi88 <sj3795789@gmail.com>
2025-02-13 16:05:30 +0530

    first commit

---
+++ index.java
@@ -0,0 +1,186 @@
+package java;
+// public class index {
+    
+//     public static void main(String args[]){
+//         System.out.print("jai shree ram");
+//     }
+// }
+
+// public class index {
+//     public static void main(String args[]){
+//         // System.out.println("jai bajrang bali");
+
+
+//         // int a = 20;
+//         // double b = a;
+//         // System.out.println(b);
+
+//         char myname = 'A';
+//         int chanegname = myname;
+//         System.out.println(chanegname);
+//     }
+
+    
+// }
+
+
+// public class index{
+//     public static void main(String[] args) {
+        
+//         int n = 5;
+//         for(int i = 1;i<=n;i++){
+//             for(int j = 1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+
+//             int spaces = 2*(n-i);
+//             for(int j= 1;j<=spaces;j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j = 1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
+
+
+//         for(int i = n;i>=1;i--){
+//             for(int j = 1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+
+//             int spaces = 2*(n-i);
+//             for(int j= 1;j<=spaces;j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j = 1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+// }
+
+
+// public class index {
+
+//     public static void main(String[] args) {
+//         int n = 5;
+//         for(int i =1;i<=n;i++){
+//             for(int j = 1;j<=n-i;j++){
+//                 System.out.print(" ");
+                
+//             }
+//             for(int j =1;j<=n;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+
+// public class index {
+
+//     public static void main(String[] args) {
+//         int n = 5;
+//         for(int i =1;i<=n;i++){
+//             for(int j = 1;j<=n-i;j++){
+//                 System.out.print(" ");
+                
+//             }
+//             for(int j =1;j<=i;j++){
+//                 System.out.print(i+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+// public class index {
+
+//     public static void main(String[] args) {
+//         int n = 5;
+//         for(int i =1;i<=n;i++){
+//             for(int j = 1;j<=n-i;j++){
+//                 System.out.print(" ");
+                
+//             }
+//             for(int j =i;j>=1;j--){
+//                 System.out.print(j);
+//             }
+//             for(int j=2;j<=i;j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+// public class index {
+
+//     public static void main(String[] args) {
+//         int n = 4;
+//         for(int i =1;i<=n;i++){
+//             for(int j = 1;j<=n-i;j++){
+//                 System.out.print(" ");
+                
+//             }
+//             for(int j =1;j<=2*i-1;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
+//         for(int i =n;i>=1;i--){
+//             for(int j = 1;j<=n-i;j++){
+//                 System.out.print(" ");
+                
+//             }
+//             for(int j =1;j<=2*i-1;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+// public class index {
+
+//     public static void main(String[] args) {
+//         int n = 5;
+//         for(int i =1;i<=n;i++){
+//             for(int j = 1;j<=n-i;j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j = 1; j<=2*i-1;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
+
+//         for(int i =n;i>=1;i--){
+//             for(int j = 1;j<=n-i;j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j = 1; j<=2*i-1;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
