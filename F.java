66e025a1d8369ae57c21349dec8fa4616cce2bc5
sureshjoshi88 <sj3795789@gmail.com>
2025-02-14 11:49:57 +0530

    changes files

---
+++ F.java
@@ -0,0 +1,11 @@
+
+public class F {
+    public static int avgnum(int a,int b, int c){
+        int sum = a+b+b/3;
+        return sum;
+    }
+    public static void main(String[] args) {
+        int value = avgnum(10,20,30);
+        System.out.println(value);
+    }
+}
---
+++ FU.java
@@ -0,0 +1,11 @@
+
+public class FU {
+    public static int avgnum(int a,int b, int c){
+        int sum = a+b+b/3;
+        return sum;
+    }
+    public static void main(String[] args) {
+        int value = avgnum(10,20,30);
+        System.out.println(value);
+    }
+}
