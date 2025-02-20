bd07c49f1d3dad04779a552a183d1fcbef23b6a8
sureshjoshi88 <sj3795789@gmail.com>
2025-02-20 15:08:02 +0530

    add new question

--- index.java
+++ index.java
@@ -193,3 +193,38 @@
 //     }
 // }
 
+// public class index {
+
+//     public static void main(String[] args) {
+//         String name = "suresh joshi";
+//         System.out.println(name);
+//     }
+// }
+
+// public class index {
+
+//     public static void main(String[] args) {
+//         int n = 5;
+//         for(int i=0;i<=n;i++){
+//             for(int j = 0;j<=i;j++){
+//                 System.out.print("x");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+public class index {
+
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=0;i<=n;i++){
+            for(int j = 0;j<=i;j++){
+                System.out.print("x");
+            }
+            
+        }
+    }
+}
