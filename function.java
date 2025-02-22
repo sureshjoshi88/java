0fcec29b333b13849d1a76951825fe7a1b651914
sureshjoshi88 <sj3795789@gmail.com>
2025-02-22 11:43:31 +0530

    add new function

--- function.java
+++ function.java
@@ -1,9 +1,21 @@
 
+// public class function {
+//      static void  cars(){
+//         System.out.println("hello how are you");
+//     }
+//     public static void main(String[] args) {
+//          cars();
+//     }
+// }
+
+
 public class function {
-     static void  cars(){
-        System.out.println("hello how are you");
+    static void myname(int a, int b){
+        int total = a+b;
+        System.out.println(total);
+    
     }
-    public static void main(String[] args) {
-         cars();
+    public static void  main(String[] args) {
+        myname(10,20);
     }
 }
