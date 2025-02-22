70d4e9dbde11caa8f949390ef07ab4c9dc64b36f
sureshjoshi88 <sj3795789@gmail.com>
2025-02-22 11:39:25 +0530

    add new function

--- function.java
+++ function.java
@@ -1,4 +1,9 @@
 
 public class function {
-    
+     static void  cars(){
+        System.out.println("hello how are you");
+    }
+    public static void main(String[] args) {
+         cars();
+    }
 }
